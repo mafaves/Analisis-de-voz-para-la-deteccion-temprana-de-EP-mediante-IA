3a80c9c57c10453ef398b0f05076e8f4a0d68df0
Marcos Aguilella <marcos.aguilella.3n@gmail.com>
2026-04-24 11:18:56 +0200

    Reorganize PyTorch trainer import to handle ImportError gracefully

--- src/training/__init__.py
+++ src/training/__init__.py
@@ -1,4 +1,9 @@
 from .cross_validation import StratifiedGroupKFold, cross_validate
-from .pytorch_trainer import PyTorchTrainer
 from .sklearn_trainer import SklearnTrainer
 from .metrics import calculate_metrics, calculate_patient_wise_metrics
+
+# PyTorch trainer requires torch
+try:
+    from .pytorch_trainer import PyTorchTrainer
+except ImportError:
+    pass
